5f5ddf2904e8466da137254183dedc7259c961e8
MehmetHuseyinDelipalta <mehmethuseyindelipalta@gmail.com>
2024-03-24 00:03:04 +0300

    Yorumları getiren yeni bir rota eklendi

---
+++ src/routes/comments.php
@@ -0,0 +1,26 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+//Tüm Yorum listesini getir
+$app->get('/comments', function (Request $request, Response $response, $args) {
+    $db = new Db();
+
+    try {
+        $db = $db->connect();
+        $comments = $db->query("SELECT * FROM comments")->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        $response->getBody()->write(json_encode($comments));
+        return $response->withHeader('Content-Type', 'application/json');
+    } catch (PDOException $e) {
+        $error = array(
+            'error' => array(
+                'text' => $e->getMessage(),
+                'code' => $e->getCode()
+            )
+        );
+        $response->getBody()->write(json_encode($error));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+});
