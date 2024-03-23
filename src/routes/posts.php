de906c9fa76653b751810935175b854ccf4d9c98
MehmetHuseyinDelipalta <mehmethuseyindelipalta@gmail.com>
2024-03-24 00:02:56 +0300

    Yeni postlar rotası eklendi

---
+++ src/routes/posts.php
@@ -0,0 +1,48 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+
+//Tüm Yorum listesini getir
+$app->get('/posts', function (Request $request, Response $response, $args) {
+    $db = new Db();
+
+    try {
+        $db = $db->connect();
+        $posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        $response->getBody()->write(json_encode($posts));
+        return $response->withHeader('Content-Type', 'application/json');
+    } catch (PDOException $e) {
+        $error = array(
+            'error' => array(
+                'text' => $e->getMessage(),
+                'code' => $e->getCode()
+            )
+        );
+        $response->getBody()->write(json_encode($error));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+});
+
+$app->get('/posts/{id}/comments', function (Request $request, Response $response, $args) {
+    $db = new Db();
+
+    try {
+        $db = $db->connect();
+        $comments = $db->query("SELECT * FROM comments WHERE postIds = " . $args['id'])->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        $response->getBody()->write(json_encode($comments));
+        return $response->withHeader('Content-Type', 'application/json');
+    } catch (PDOException $e) {
+        $error = array(
+            'error' => array(
+                'text' => $e->getMessage(),
+                'code' => $e->getCode()
+            )
+        );
+        $response->getBody()->write(json_encode($error));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+});
