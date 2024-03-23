b0e5278af0b6ae29e7232dd5cfd51b0b8724be48
MehmetHuseyinDelipalta <mehmethuseyindelipalta@gmail.com>
2024-03-23 18:53:04 +0300

    Yeni veritabanı bağlantısı sınıfı eklendi

---
+++ src/config/db.php
@@ -0,0 +1,16 @@
+<?php
+class Db
+{
+    private $dbhost = 'localhost';
+    private $dbuser = 'root';
+    private $dbpass = '';
+    private $dbname = 'blogs';
+
+    public function connect()
+    {
+        $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname;charset=utf8";
+        $dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
+        $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $dbConnection;
+    }
+}
